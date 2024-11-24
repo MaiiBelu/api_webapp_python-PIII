2e19fa4057a62a88402aa04c9cd6df516dad377b
Mayra Cruz <mbc15b@gmail.com>
2024-11-24 20:51:36 -0300

    persona_modif

--- ejercicios_practica/persona.py
+++ ejercicios_practica/persona.py
@@ -54,47 +54,18 @@ def report(limit=0, offset=0):
     return json_result_list
 
 def dashboard():
-    # Obtener los últimos 250 registros del paciente
-    # ordenado por fecha, obteniedo los últimos 250 registros
     query = db.session.query(Persona).order_by(Persona.name.desc())
-    #query = query.limit(250)
     query_results = query.all()
 
     if query_results is None or len(query_results) == 0:
-        # No data register
-        # Bug a proposito dejado para poner a prueba el traceback
-        # ya que el sistema espera una tupla
         return []
 
     x = []
     y = []
 
-    # De los resultados obtenidos tomamos el tiempo y las puslaciones pero
-    # en el orden inverso, para tener del más viejo a la más nuevo registro
+
     for resultados in query_results:
         x.append(resultados.name)
         y.append(resultados.age)
 
     return x, y
-
-if __name__ == "__main__":
-    print("Test del modulo heart.py")
-
-    # Crear una aplicación Flask para testing
-    # y una base de datos fantasma (auxiliar o dummy)
-    # Referencia:
-    # https://stackoverflow.com/questions/17791571/how-can-i-test-a-flask-application-which-uses-sqlalchemy
-    app = Flask(__name__)
-    app.config['TESTING'] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///testdatabase.db"
-    # Bindear la DB con nuestra app Flask
-    db.init_app(app)
-    app.app_context().push()
-
-    db.create_all()
-
-    # Aquí se puede ensayar todo lo que necesitemos con nuestra DB
-    # ...
-
-    db.session.remove()
-    db.drop_all()
